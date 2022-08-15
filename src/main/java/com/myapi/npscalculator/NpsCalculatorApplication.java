71c03c567ae535f3992a09dce4fb0e2fc28e0c31
luizfelipetorres <fellipe116@gmail.com>
2022-08-15 15:57:34 -0300

    First commit

---
+++ src/main/java/com/myapi/npscalculator/NpsCalculatorApplication.java
@@ -0,0 +1,20 @@
+package com.myapi.npscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NpsCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpsCalculatorApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/myapi/npscalculator/NpsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapi.npscalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpsCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
