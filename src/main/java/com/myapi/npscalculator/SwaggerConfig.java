fd018fd068c6f3ff615f8a8c7eefc3bbb861d232
luizfelipetorres <fellipe116@gmail.com>
2022-08-22 11:24:00 -0300

    Add information to Swagger

--- src/main/java/com/myapi/npscalculator/SwaggerConfig.java
+++ src/main/java/com/myapi/npscalculator/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package com.myapi.npscalculator;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.RequestHandledEvent;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,10 +19,22 @@ public class SwaggerConfig {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any())
-        .build();
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.myapi.npscalculator.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(information());
+    }
+
+    private ApiInfo information() {
+        return new ApiInfo("NPS CALCULATOR JAVA",
+                "Nps calculator made with Java 11, Maven, Spring Boot, Hibernate, Validation, Springfox and Postgres",
+                "1",
+                "", new Contact("Luiz Felipe Tôrres", "https://github.com/luizfelipetorres", "fellipe116@gmail.com"),
+                "No license",
+                "",
+                Collections.emptyList());
     }
     
+
 }
